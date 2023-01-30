fc469adc9163e5219f2b6ea8102c1e52a6096bc5
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-01-30 21:50:42 +0200

    array type

---
+++ basic/array_type/index.js
@@ -0,0 +1,16 @@
+"use strict";
+let numbers = [1, 2, 3, 4, 5];
+let products = ["Hat", "Shirt", "Jeans"];
+let mixData1 = ["Hi", 25, "Hello"];
+let mixData2 = ["Hi", 25, "Hello", true];
+let myNums = [
+    [1, 2],
+    [2, 3]
+];
+let myStrings = [
+    ["hi", "hello"], ["there"]
+];
+let myArr = [
+    [12, "hi"],
+    ["hello", 3]
+];
---
+++ basic/array_type/index.ts
@@ -0,0 +1,28 @@
+//array of numbers
+let numbers: number[] = [1,2,3,4,5];
+
+// array of strings
+let products: string[] = ["Hat", "Shirt", "Jeans"];
+
+//array os strings & numbers
+let mixData1: (string | number)[] = ["Hi", 25, "Hello"];
+
+//array os strings & numbers & boolean
+let mixData2: (string | number | boolean)[] = ["Hi", 25, "Hello", true];
+
+//nested number type array
+let myNums: number[][] = [
+[1,2],
+[2,3]
+];
+
+//nested string type array
+let myStrings: string[][] = [
+    ["hi", "hello"],["there"]
+];
+
+//nestwed array number & string
+let myArr: (number | string)[][] = [
+    [12, "hi"],
+    ["hello", 3]
+]
