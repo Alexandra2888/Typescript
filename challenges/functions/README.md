7034637917c35ee32ceaa874562971a08978172f
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-02-14 17:08:29 +0200

    function challenges

--- challenges/functions/README.md
+++ challenges/functions/README.md
@@ -2,3 +2,6 @@
 1. Create a function that returns the multiplication of two arbitrary numbers and returns a type error when we pass a string.
 2. Simulate a type error for the return value of a function that has an arbitrary type.
 3. Create an error function that returns the addition of a number and a boolean type.
+5. Create a function that makes words pluralized.
+6. Write a function that adds one to a number or substracts one from a number depending on a boolean condition.
+7. Create a function that takes an array of numbers and just returns the array. But if anything else is passed within that array like a string/boolean/anything else throws an error.
--- challenges/functions/index.ts
+++ challenges/functions/index.ts
@@ -3,18 +3,43 @@ function multiply (a: number, b: number):number {
     return a * b;
 }
 
+//2
 // multiply(2, "3");
 
-//2
+//3
 function emailList(email: string):string {
     return email;
 }
 // emailList(5);
 
-//3
+//4
 // function mySum(s: number, t: boolean): number {
 //     return s + t
 // }
 
 
-//4
+//5
+let pluralize = function (str:string):string {
+    return str + "s";
+}
+pluralize("cake");
+
+//6
+function decideOPeration(num:number, bool:boolean):number {
+    if (bool) {
+        return num + 1;
+    }
+    return num - 1;
+}
+
+decideOPeration(10, true);
+decideOPeration(10, false);
+
+// bool ? num + 1 : num -1;
+
+//7
+function myArr2(arr: number[]):number[] {
+    return arr;
+}
+myArr2([1, 2, 3]);
+// myArr2([1, '2', 3]); //type eror
